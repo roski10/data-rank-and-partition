4b1fd58f5a4b8673589ffb4867e278070e84f8a7
rohan mehra <rohanyyz@gmail.com>
2022-10-22 16:32:13 +0100

    Completed exercise

--- queries.py
+++ queries.py
@@ -1,8 +1,27 @@
 # pylint:disable=C0111,C0103
-
+import sqlite3
+conn = sqlite3.connect('data/ecommerce.sqlite')
+db = conn.cursor()
 def order_rank_per_customer(db):
-    pass  # YOUR CODE HERE
+    query = '''SELECT
+    Orders.OrderID,
+    Orders.CustomerID,
+    Orders.OrderDate,
+    RANK() OVER (PARTITION BY Orders.CustomerID ORDER BY Orders.OrderDate) AS OrderRank
+    FROM Orders; '''
+    db.execute(query)
+    results = db.fetchall()
+    return results
+    # YOUR CODE HERE
 
 
 def order_cumulative_amount_per_customer(db):
-    pass  # YOUR CODE HERE
+    query = '''SELECT DISTINCT
+    Orders.OrderID, Orders.CustomerID, Orders.OrderDate,
+    SUM(UnitPrice*Quantity) OVER (PARTITION BY Orders.CustomerID ORDER BY Orders.OrderDate) AS Order_cum_amount
+    FROM Orders
+    JOIN OrderDetails
+    ON Orders.OrderID = OrderDetails.OrderID; '''
+    db.execute(query)
+    results = db.fetchall()
+    return results
